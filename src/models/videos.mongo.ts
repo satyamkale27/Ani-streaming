d595d0e47d6d5cf238c7e5306adfd958946df288
satyam <satyamkale271@gmail.com>
2025-02-04 19:13:44 +0530

    schema relation video to comments

--- src/models/videos.mongo.ts
+++ src/models/videos.mongo.ts
@@ -2,13 +2,15 @@ import mongoose, { Schema, Document } from "mongoose";
 interface IVideo extends Document {
   EpisodeNo: Number;
   url: string;
-  comments: String;
+  comments: mongoose.Schema.Types.ObjectId[];
 }
 
 const videoScheme: Schema = new mongoose.Schema({
   EpisodeNo: { type: Number, required: true },
   url: { type: String, required: true },
-  comments: { type: String },
+  comments: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Comment", required: true },
+  ],
 });
 
 const AnimeVideo = mongoose.model<IVideo>("AnimeVideo", videoScheme);
