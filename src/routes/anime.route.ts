7d7f55ceca73f771d8c664920cd389a178675bb1
satyam <satyamkale271@gmail.com>
2025-02-04 19:46:00 +0530

    createComment route

--- src/routes/anime.route.ts
+++ src/routes/anime.route.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { addAnime, addDataAndStartViedoTranscoding, generatePresignedUrlsForFileUpload } from "../controllers/anime.controller";
+import {
+  addAnime,
+  addDataAndStartViedoTranscoding,
+  createComment,
+  generatePresignedUrlsForFileUpload,
+} from "../controllers/anime.controller";
 import upload from "../middlewares/multer";
 
 const animeRouter = Router();
 animeRouter.post("/uploadanime", upload.single("animeImage"), addAnime);
-animeRouter.post("/generate-presigned-urls", generatePresignedUrlsForFileUpload)
-animeRouter.post("/addDataAndStartViedoTranscoding", addDataAndStartViedoTranscoding)
+animeRouter.post("/createComment", createComment);
+animeRouter.post(
+  "/generate-presigned-urls",
+  generatePresignedUrlsForFileUpload
+);
+animeRouter.post(
+  "/addDataAndStartViedoTranscoding",
+  addDataAndStartViedoTranscoding
+);
 
 export default animeRouter;
